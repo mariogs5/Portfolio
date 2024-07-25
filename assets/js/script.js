f62840fafb90b7bde49b040019b27a08208b3848
Mario Garcia Sutil <mariosutil2003@gmail.com>
2024-07-25 02:10:08 +0200

    Grid Rework

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function openPopup(popupId) {
+    document.getElementById(popupId).style.display = "block";
+}
+
+function closePopup(popupId) {
+    document.getElementById(popupId).style.display = "none";
+}
