5a8cecab9b088f2f75746912b2c610a902f301f9
Mario Garcia Sutil <mariosutil2003@gmail.com>
2024-07-27 04:23:47 +0200

    Description & Alien Extraction Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 function openPopup(popupId) {
     document.getElementById(popupId).style.display = "block";
+    document.body.classList.add("no-scroll");
 }
 
 function closePopup(popupId) {
     document.getElementById(popupId).style.display = "none";
+    document.body.classList.remove("no-scroll");
 }
